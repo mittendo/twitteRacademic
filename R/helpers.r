3a2d22892e0312be173b1df15a3bc8a953efce02
Volker Mittendorf <mittendo@uni-wuppertal.de>
2021-03-11 11:01:18 +0100

    - R CMD Check Warnings checked and solved - No more lists in tibbles

--- R/helpers.r
+++ R/helpers.r
@@ -1,29 +1,37 @@
 # helpers.r
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
 
 tweetbindcol <- function(.data, subdf) {
   nametest <- deparse(substitute(subdf))
-  test <- .data[[substitute(subdf)]]  %>% rename_with(~ paste0(nametest , "_", .x))
+  test <- .data[[substitute(subdf)]]  %>% dplyr::rename_with(~ paste0(nametest , "_", .x))
 
   # rename cols with df-name
   # test <- test %>% rename_with(~ paste0(. , "_", .x))
-   .data <- .data %>% select(- {{subdf}})
+   .data <- .data %>% dplyr::select(- {{subdf}})
   .data <- .data %>%
-    bind_cols(test)
+    dplyr::bind_cols(test)
   return(.data)
 }
 
+#' @importFrom rlang .data
 unnestcol <- function(.data, item, ...){
-  newidname <- paste0(substitute(.data), "_id")
+
+  nametest <- deparse(substitute(.data))
+
+  newidname <- paste0(nametest, "_id")
+
+   .data <- dplyr::select(.data, c( id, {{item}}))
    idrename <- colnames(.data) == "id"
-  .data <- dplyr::select(.data, c( id, ({{item}})))
 
   colnames(.data)[idrename] <- newidname
-  .data <- unnest(.data, {{item}})
+  .data <- tidyr::unnest(.data, {{item}})
 
   return(.data)
 }
 
 ## Helpers for Postprocessing of resource data
+#' @importFrom rlang .data
 check_col <- function(.data, col) {
 
   test <- sum(names(.data)==col) > 0
@@ -31,11 +39,13 @@ check_col <- function(.data, col) {
   return(test)
 }
 
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
 make_tidyresusers <- function(.data) {
 
   if(check_col(.data, "public_metrics")) .data <- .data %>%
-      tweetbindcol(public_metrics) %>% rename_with(~ paste0("user_", .x), starts_with("public"))
-  .data <- .data %>% rename(user_created_at = created_at)
+      tweetbindcol(public_metrics) %>% dplyr::rename_with(~ paste0("user_", .x), dplyr::starts_with("public"))
+  .data <- .data %>% dplyr::rename(user_created_at = created_at)
 
   if(check_col(.data, "entities")) .data <- .data %>%
       tweetbindcol(entities)
@@ -47,30 +57,30 @@ make_tidyresusers <- function(.data) {
 
   if(check_col(.data, "entities_description_hashtags")) {
     description_hashtags <- .data %>% unnestcol(entities_description_hashtags)
-  } else description_hashtags <- tibble()
+  } else description_hashtags <- tibble::tibble()
 
   if(check_col(.data, "entities_description_cashtags"))    {
     description_cashtags <- .data %>% unnestcol(entities_description_cashtags)
 
-    } else description_cashtags <- tibble()
+    } else description_cashtags <- tibble::tibble()
   if(check_col(.data, "entities_description_mentions"))    {
     description_mentions <- .data %>% unnestcol(entities_description_mentions)
-    } else description_mentions <- tibble()
+    } else description_mentions <- tibble::tibble()
   if(check_col(.data, "entities_description_urls"))    {
     description_urls <- .data %>% unnestcol(entities_description_urls) %>%
-      mutate(source = "description")
-    } else description_urls <- tibble()
+      dplyr::mutate(source = "description")
+    } else description_urls <- tibble::tibble()
 
   if(check_col(.data, "entities_url")) {
     .data <- .data %>%    tweetbindcol(entities_url)
-    urls <- .data %>% unnestcol(entities_url_urls) %>% mutate(source = "url")
+    urls <- .data %>% unnestcol(entities_url_urls) %>% dplyr::mutate(source = "url")
   } else {
-    entities_url <- tibble()
+    entities_url <- tibble::tibble()
   }
 
-  urls <- bind_rows(urls, description_urls)
+  urls <- dplyr::bind_rows(urls, description_urls)
 
-  users <- .data %>% select(- starts_with("entities"))
+  users <- .data %>% dplyr::select(- dplyr::starts_with("entities"))
 
   return(list(users = users,
               urls = urls,
@@ -83,12 +93,16 @@ make_tidyresusers <- function(.data) {
 }
 
 
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
 make_tidyrespoll <- function(.data) {
   if(dim(.data)[1]>0) .data <- .data %>% unnestcol(options)
 return(.data)
 }
 
 
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
 make_tidyrestweets <- function(.data){
   # postprocess tweet-data
   # bind attached data frames
@@ -100,68 +114,78 @@ make_tidyrestweets <- function(.data){
     tweetbindcol(attachments)
 
   if(sum(names(.data)=="geo")>0) {
-    .data <- .data %>%   tweetbindcol(geo)
+     .data <- .data %>%   tweetbindcol(geo) # %>%
+    #   tweetbindcol(geo_coordinates) %>% tweetbindcol(coordinates)
+     .data <- .data %>% tweetbindcol(geo_coordinates)
+     .data <- .data %>%
+       dplyr::mutate(lon=purrr::map_dbl(geo_coordinates_coordinates,1, .default=NA),
+              lat=purrr::map_dbl(geo_coordinates_coordinates,2, .default=NA)) %>%
+       dplyr::select(-geo_coordinates_coordinates)
   }
 
-  if(check_col(.data, "geo_coordinates")) {
-    geo_coordinates  <- .data %>% unnestcol(geo_coordinates)
-  } else {
-    geo_coordinates <- tibble()
-  }
 
   # unnest subdf's, referenced_tweets,
   #                 entities_hashtags, entities_cashtags, entities_mentions,
   #                 entities_annotations, entities_urls,
   #                 geo_coordinates
   if(check_col(.data, "entities_hashtags"))   {
+    #print(.data$entities_hashtags)
     hashtags <- .data %>% unnestcol(entities_hashtags)
   } else {
-    hashtags <- tibble()
+    hashtags <- tibble::tibble()
   }
 
   if(check_col(.data, "entities_cashtags"))   {
     cashtags <- .data %>% unnestcol(entities_cashtags)
   } else {
-    cashtags <- tibble()
+    cashtags <- tibble::tibble()
   }
+
+
+ if(check_col(.data, "referenced_tweets")) {
+   referenced_tweets <- .data %>% unnestcol(referenced_tweets)
+
+ }
 
   if(check_col(.data, "entities_mentions"))   {
     mentions <- .data %>% unnestcol(entities_mentions)
   } else {
-    mentions <- tibble()
+    mentions <- tibble::tibble()
   }
 
   if(check_col(.data, "entities_annotations"))   {
     annotations <- .data %>% unnestcol(entities_annotations)
   } else {
-    annotations <- tibble()
+    annotations <- tibble::tibble()
   }
 
   if(check_col(.data, "entities_urls"))   {
   urls <- .data %>% unnestcol(entities_urls)
   } else {
-    urls <- tibble()
+    urls <- tibble::tibble()
   }
 
-  tweets <- .data %>% select(- starts_with("entities"))
+
+
+  if(check_col(.data, "attachments_media_keys")) {
+    #attachments_media_keys <- unnestcol(attachments_media_keys)
+   .data <-  .data %>% dplyr::mutate(attachments_media_keys =
+                       purrr::map_chr(attachments_media_keys, 1, .default=NA))
+  }
+
+  tweets <- .data %>% dplyr::select(- dplyr::starts_with("entities"), -referenced_tweets)
 
 
      df <- list(tweets = tweets,
                 hashtags = hashtags,
                 mentions = mentions,
                 annotations = annotations,
-                urls = urls,
-                geo_coordinates = geo_coordinates)
+                referenced_tweets = referenced_tweets,
+    #            attachments_media_keys = attachments_media_keys,
+                urls = urls)
+            #    geo_coordinates = geo_coordinates)
      return(df)
 
 
 }
 
-
-# data -> bind : c(public_metrics, entities, geo, attachments, geo_coordinates)
-# data -> unnest : c(referenced_tweets, hashtags, cashtags, mentions, annotations,urls,media_keys, coordinates)
-# includes_tweets -> bind : c(public_metrics, entities, attachments)
-# includes_tweets -> unnest : c(referenced_tweets, hashtags, cashtags, mentions, annotations,urls,media_keys)
-# includes_media -> flat
-# includes_places -> flat
-# includes_polls -> unnest : c(options)
--- README.md
+++ README.md
@@ -84,6 +84,15 @@ Please cite this package, if you have used it in your works:
 
 `citation("twitteRacademic")`
 
+
+## TO-DO
+
+
+- Use a sample parameter for test purposes (retreive only 3.000 tweets not an unpredictable amount of tweets),
+- Write a wrapper for search queries (from, hashtags, filters like: has attachment, has poll and so on),
+- Write some convenience functions to retrieve tidy data-frames for standard purposes.
+
+
 ## License
 
 GPL-3
